33c21f7c318bf1e2c5e0d305e46a72f3448bb88f
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-15 11:56:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+
+function secondHighest(arr) {
+    if (arr.length < 2 || new Set(arr).size === 1) {
+      return -Infinity;
+    }
+  
+    let firstMax = -Infinity;
+    let secondMax = -Infinity;
+  
+    for (let num = 0; num < arr.length; num++) {
+      if (arr[num] > firstMax) {
+        secondMax = firstMax;
+        firstMax = arr[num];
+      } else if (arr[num] < firstMax && arr[num] > secondMax) {
+        secondMax = arr[num];
+      }
+    }
+    return secondMax;
+  }
+  
